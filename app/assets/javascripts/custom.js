62c289c38bd3717332951f86a538dc2237832928
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-02-02 23:39:50 -0600

    Continues work on About page.

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+	$('.tech-link').on('click', function(e) {
+		$('.container div').empty();
+		$('.about-tech').fadeIn('slow');
+		e.stopPropagation();
+	});
+
+	$('.personality-link').on('click', function(e) {
+		$('.about-personality').fadeToggle('slow');
+		e.stopPropagation();
+	});
+
+	$('.care-link').on('click', function(e) {
+		$('.about-care').fadeToggle('slow');
+		e.stopPropagation();
+	});
+
+	$('.edu-link').on('click', function(e) {
+		$('.about-education').fadeToggle('slow');
+		e.stopPropagation();
+	});
+
+	$('.skills-link').on('click', function(e) {
+		$('.about-skills').fadeToggle('slow');
+		e.stopPropagation();
+	});
+
+});
