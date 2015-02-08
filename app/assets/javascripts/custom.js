64bedab797895a5eac52eaaf2ff4d9e1326cbb89
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-02-07 23:03:21 -0600

    Minor bubbling fix for About page.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,31 +3,37 @@ $(document).ready(function() {
 	$('.tech-link').on('click', function(event) {
 		$('.container').empty();
 		$('.container').append($('.about-tech').fadeToggle('slow'));
+		event.preventDefault();
 	});
 
 	$('.personality-link').on('click', function(event) {
 		$('.container').empty();
 		$('.container').append($('.about-personality').fadeToggle('slow'));
+		event.preventDefault();
 	});
 
 	$('.care-link').on('click', function(event) {
 		$('.container').empty();
 		$('.container').append($('.about-care').fadeToggle('slow'));
+		event.preventDefault();
 	});
 
 	$('.edu-link').on('click', function(event) {
 		$('.container').empty();
 		$('.container').append($('.about-education').fadeToggle('slow'));
+		event.preventDefault();
 	});
 
 	$('.skills-link').on('click', function(event) {
 		$('.container').empty();
 		$('.container').append($('.about-skills').fadeToggle('slow'));
+		event.preventDefault();
 	});
 
 	$('.fun-link').on('click', function(event) {
 		$('.container').empty();
 		$('.container').append($('.about-fun').fadeToggle('slow'));
+		event.preventDefault();
 	});
 
 });
