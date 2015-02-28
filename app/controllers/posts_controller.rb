6643d8d1877debb12bf344cbef2b90ff10bf9827
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-02-27 22:39:49 -0600

    Sets up basic Admin/Posts relationships.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_admin!, only: [:new, :create, :edit, :destroy]
+  before_action :set_admin, only: [:new, :create, :edit, :destroy]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
