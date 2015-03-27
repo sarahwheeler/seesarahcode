7b95615ce1cc8ed15c6168a159a41f48f55307e5
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-03-26 22:43:57 -0500

    Adds published status to post form.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,8 @@ class AdminController < ApplicationController
 	before_action :authenticate_admin!, only: [:superpanel]
 
 	def superpanel
-		 @posts = Post.all
+		 @posts = Post.where(is_published: true)
+		 @unpublished_posts = Post.where(is_published: false)
 		 @courses = Course.all
 	end
 
