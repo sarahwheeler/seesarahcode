5a592dcae826eb230d95845d4f53c12d82be6038
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-04-27 22:38:09 -0500

    Working commit on Treehouse badges via AJAX request.

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -2,13 +2,17 @@ $.ajax({
   type: 'GET',
   url: 'http://teamtreehouse.com/sarahwheeler.json',
   data: {
-      format: 'json'
-   }
+    format: 'json'
+  }
 }).success(function(data) {
-  console.log(data);
   var points = data["points"]["total"];
   console.log(points);
   $('#treehouse-pts').text(commaSeparatedNumber(points));
+  $('#treehouse-bdgs').text(data.badges.length);
+  var badges = badgeGrabber(data) // returns icon_urls
+  badgeImageInsert(badges); //insert badge images
+  badgeAltInsert(badges); //insert badge alt text
+  // send data to language bubble chart
 });
 
 function commaSeparatedNumber(val){
@@ -16,4 +20,25 @@ function commaSeparatedNumber(val){
     val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
   }
   return val;
+}
+
+function badgeGrabber(profile) {
+	var icon_urls = [] // array [,] within array
+	var lastFive = profile.badges.sort(function(a, b) {
+    a = new Date(a.earned_date);
+    b = new Date(b.earned_date);
+    return a>b ? -1 : a<b ? 1 : 0;
+	}).slice(0,5);
+	lastFive.forEach(function(badge) {
+		 icon_urls << [badge.name, badge.icon_url]
+	});
+	return icon_urls;
+}
+
+function badgeImageInsert(badges) {
+
+}
+
+function badgeAltInsert(badges) {
+
 }
