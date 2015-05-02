9166c64d90d7139cde4da184effa251dd0a0f72c
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-05-01 22:52:49 -0500

    Working commit for Recently Read.

--- app/assets/javascripts/resume.js
+++ app/assets/javascripts/resume.js
@@ -1,18 +1,9 @@
 $(document).ready(function() {
 
-	$('.bk-circle').on('click', function(event) {
-		$('div.bk-circle').removeClass('active');
-		$(this).addClass('active');
-		if ($(this).hasClass('.bk1')) {
-			$('.book-img').empty();
-			$('.book-img').append("<img src='assets/recently_read/cravens_building_apps_ember.jpg' title='Building Web Apps with Ember.js by Jesse Cravens & Thomas Brady'>");
-		} else if ($(this).hasClass('.bk2')) {
-			$('.book-img').empty();
-			$('.book-img').append("<img src='assets/recently_read/mcfarland_jj_missing_manual.jpeg' title=''>");
-		} else if ($(this).hasClass('.bk3')===true) {
-			$('.book-img').empty();
-			$('.book-img').append("<img src='assets/recently_read/fernandez_rails4_way.jpg' title=''>");
-		}
-	});
+	$('.book-img').append("<img src='assets/recently_read/cravens_building_apps_ember.jpg' title='Building Web Apps with Ember.js by Jesse Cravens & Thomas Brady'>").fadeIn( 400 ).delay( 3000 ).fadeOut( 400 );
+
+	$('.book-img').append("<img src='assets/recently_read/mcfarland_jj_missing_manual.jpeg' title='JavaScript and jQuery: The Missing Manual by Scott McFarland'>").fadeIn( 400 ).delay( 3000 ).fadeOut( 400 );
+
+	$('.book-img').append("<img src='assets/recently_read/fernandez_rails4_way.jpg' title='The Rails 4 Way by Obie Fernandez'>").fadeIn( 400 ).delay( 3000 ).fadeOut( 400 ).empty();
 
 });
