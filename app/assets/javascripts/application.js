0ad1bae955c1a232fdeb7fbda10284a92d340a60
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-05-06 23:45:56 -0500

    Finishes Recently Read image slider. So bad ass.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,4 @@
 //= require_tree .
 //= require bootstrap
 //= require tinymce
+//= require jquery.slick
--- app/assets/javascripts/resume.js
+++ app/assets/javascripts/resume.js
@@ -1,9 +1,14 @@
-$(document).ready(function() {
-
-	$('.book-img').append("<img src='assets/recently_read/cravens_building_apps_ember.jpg' title='Building Web Apps with Ember.js by Jesse Cravens & Thomas Brady'>").fadeIn( 400 ).delay( 3000 ).fadeOut( 400 );
-
-	$('.book-img').append("<img src='assets/recently_read/mcfarland_jj_missing_manual.jpeg' title='JavaScript and jQuery: The Missing Manual by Scott McFarland'>").fadeIn( 400 ).delay( 3000 ).fadeOut( 400 );
-
-	$('.book-img').append("<img src='assets/recently_read/fernandez_rails4_way.jpg' title='The Rails 4 Way by Obie Fernandez'>").fadeIn( 400 ).delay( 3000 ).fadeOut( 400 ).empty();
-
+$(document).ready(function(){
+  $('.book-img').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  autoplay: true,
+	  autoplaySpeed: 5000,
+	  dots: true,
+	  infinite: true,
+	  speed: 700,
+	  fade: true,
+	  cssEase: 'linear'
+	});
 });
+				
