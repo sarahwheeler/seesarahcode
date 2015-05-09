f3cc6d96c08eea3f5a308fc10fd4d99ff2b59966
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-05-09 01:23:23 -0500

    Able to draw a total circle, working on multiple bubbles.

--- app/assets/javascripts/treehouse_bubbles.js
+++ app/assets/javascripts/treehouse_bubbles.js
@@ -8,30 +8,37 @@ $(document).ready(function() {
 	  								.attr('width', width)
 	  								.attr('id', 'bubble-frame');
 
-	d3.json('https://teamtreehouse.com/sarahwheeler.json', function(error, json) {
+	d3.json('https://teamtreehouse.com/sarahwheeler.json', 
+					function(error, json) {
     if (error) return console.warn(error);
     data = json;
-    bubbles = bubbleFrame.selectAll('circle').data(data)
+    var points = [];
+    points.push(data.points);
+    for (var value in points) {
+    	var point_total = points[value];
+    	bubbles = bubbleFrame.selectAll('circle')
+    											.data(point_total)
 	  											.enter()
 	  											.append('circle');
-	  bubbles.attr('r', function(d) { return d })
-	  				.attr('cy', 200)
-	  				.attr('cx', 200)
+	  	bubbles.attr('r', function(d) { return Math.abs(d)/65 })
+	  				.attr('cx', function(d) {return Math.max(0 + padding, Math.random() * width - padding)})
+          	.attr('cy', function(d) {return Math.max(0 + padding, Math.random() * height - padding)})
 	  				.attr('fill', '#000')
+    }
   });
-
-	function createTreehouseBubbles(data) {
-		bubbles = bubbleFrame.selectAll('circle').data(data)
-	  											.enter()
-	  											.append('circle');
-	  bubbles.attr('r', function(d) { return d })
-	  				.attr('cy', 200)
-	  				.attr('cx', 200)
-	  				.style('fill', '#000')
-	}
-
 });
 
 //	  	for (var value in d.points) { // value is the language
 //	  		return d.points[value] // returns the points
 //	  	}
+
+// This works for one circle: 
+//points.push(data.points);
+//    bubbles = bubbleFrame.selectAll('circle')
+//    											.data(points)
+//	  											.enter()
+//	  											.append('circle');
+//	  bubbles.attr('r', function(d) { return Math.abs(d.total)/95 })
+//	  				.attr('cy', 200)
+//	  				.attr('cx', 200)
+//	  				.attr('fill', '#000')
