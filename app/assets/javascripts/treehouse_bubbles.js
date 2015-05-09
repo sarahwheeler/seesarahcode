cf6b2a7ac9a86cb7a844bc8ec0504a26058851a5
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-05-08 23:45:33 -0500

    Working commit for bubble chart.

--- app/assets/javascripts/treehouse_bubbles.js
+++ app/assets/javascripts/treehouse_bubbles.js
@@ -1,22 +1,37 @@
 $(document).ready(function() {
+	var height = 500;
+	var width = 800;
+	var padding = 50;
+	bubbleFrame = d3.select("#th-bubbles-wrapper")
+	  								.append('svg')
+	  								.attr('height', height)
+	  								.attr('width', width)
+	  								.attr('id', 'bubble-frame');
 
 	d3.json('https://teamtreehouse.com/sarahwheeler.json', function(error, json) {
     if (error) return console.warn(error);
     data = json;
-    console.log(data);
-    createTreehouseBubbles(data);
+    bubbles = bubbleFrame.selectAll('circle').data(data)
+	  											.enter()
+	  											.append('circle');
+	  bubbles.attr('r', function(d) { return d })
+	  				.attr('cy', 200)
+	  				.attr('cx', 200)
+	  				.attr('fill', '#000')
   });
 
 	function createTreehouseBubbles(data) {
-		var bubbleFrame = d3.select("#th-bubbles-wrapper")
-	  								.append('svg')
-	  								.attr('id', 'bubble-frame');
-
-		bubbles = bubbleFrame.selectAll('circle')
-	  											.data(data)
+		bubbles = bubbleFrame.selectAll('circle').data(data)
 	  											.enter()
 	  											.append('circle');
-	  bubbles.attr('r', function(d) { return console.log(d) });
+	  bubbles.attr('r', function(d) { return d })
+	  				.attr('cy', 200)
+	  				.attr('cx', 200)
+	  				.style('fill', '#000')
 	}
 
 });
+
+//	  	for (var value in d.points) { // value is the language
+//	  		return d.points[value] // returns the points
+//	  	}
