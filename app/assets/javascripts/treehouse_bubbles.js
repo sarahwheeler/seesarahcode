1a060cb28d888649c9b6a906edaecffc204aacdd
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-05-10 17:11:31 -0500

    Adds color to bubbles, and attaches text labels to circles (not showing up yet, though).

--- app/assets/javascripts/treehouse_bubbles.js
+++ app/assets/javascripts/treehouse_bubbles.js
@@ -1,44 +1,49 @@
 $(document).ready(function() {
-	var height = 600;
-	var width = 900;
+	var height = 400;
+	var width = 600;
 	var padding = 50;
-	bubbleFrame = d3.select("#th-bubbles-wrapper")
+	var diameter = 600;
+	
+	svg = d3.select("#th-bubbles-wrapper")
 	  								.append('svg')
-	  								.attr('height', height)
-	  								.attr('width', width)
+	  								.attr('height', diameter)
+	  								.attr('width', diameter)
 	  								.attr('id', 'bubble-frame');
+
+	bubble = d3.layout.pack()
+						.size([diameter, diameter])
+						.value(function(d) { return d.points })
+						.padding(3);
+
 
 	d3.json('https://teamtreehouse.com/sarahwheeler.json', 
 					function(error, json) {
     if (error) return console.warn(error);
     var data = {"points": []};
     for (var value in json.points) {
-    	if (value !== "total" && value !== "Business" && value !== "Design") {
+    	if (value !== "total" && value !== "Business") {
     		var point = json.points[value];
     		var subject = value;
     		data["points"].push({"subject": subject, "points": point});
     	}
     }
-    bubbles = bubbleFrame.selectAll('circle')
+    var nodes = bubble.nodes(data.points);
+
+    bubbles = svg.selectAll('circle')
     											.data(data.points)
 	  											.enter()
 	  											.append('circle');
 	  bubbles.attr('r', function(d) { return Math.abs(d.points)/35 })
-	  				.attr('cx', function(d) {return Math.max(0 + padding, Math.random() * width - padding)})
+	  .attr('cx', function(d) {return Math.max(0 + padding, Math.random() * width - padding)})
           	.attr('cy', function(d) {return Math.max(0 + padding, Math.random() * height - padding)})
 	  				.attr('fill', '#15B89A')
-	  				.attr('stroke', '#000');
-	  var text = bubbleFrame.selectAll("text")
-                        .data(data.points)
-                        .enter()
-                        .append("text");
-    var textLabels = text
-                 .attr("x", function(d) { return d.cx; })
-                 .attr("y", function(d) { return d.cy; })
-                 .text( function (d) { return ; })
-                 .attr("font-family", "sans-serif")
-                 .attr("font-size", "20px")                 
-                 .attr("fill", "purple");
+	  				.attr('stroke', '#000')
+	  				.attr('class', function(d) {return d.subject.toLowerCase();});
+
+    bubbles.append("svg:text")
+       .attr("text-anchor", "middle")
+       .text(function(d) { return d.subject; })
+       .attr("fill", "black");
   });
 });
 
