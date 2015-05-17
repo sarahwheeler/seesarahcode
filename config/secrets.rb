8c8a307e5c82f8fb69dd71c8adf40ebd4bef9678
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-05-17 15:57:49 -0500

    Forcing secrets.

---
+++ config/secrets.rb
@@ -0,0 +1,12 @@
+# TUMBLR
+	#consumer_key 
+		@key =  'GArZmkvh9r4OX46HD3QWZY3JZMYjmpdwPjrmgtcNYJXbKsGqXX'
+
+	#consumer_secret
+		@secret = 'OhGJQ2pGMzWyZcDsVLc6OWC9oQmNUf3itDP75ZDgmUSuIUykfQ'
+
+	#oauth_token
+		@oauth_token = '8Z3oisWoVhKIavLHoeRrToKrHXATrgGshxRJ6jy5y5v109yNpl'
+
+	#oauth_token_secret
+		@oauth_token_secret = 'ZYKJvHhNbve1JW0Y6l32auL2XkOepe7GA75VMgnaxasSKeGKJl'
