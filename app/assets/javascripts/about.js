f75c476349dadc0ea44830cf3efd745b306636e5
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-05-19 23:50:58 -0500

    Adds Lupin slideshow because I'm a pet-narcissist.

---
+++ app/assets/javascripts/about.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.lupin-img').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  autoplay: true,
+	  autoplaySpeed: 5000,
+	  dots: true,
+	  infinite: true,
+	  speed: 700,
+	  fade: true,
+	  cssEase: 'linear'
+	});
+});
+				
