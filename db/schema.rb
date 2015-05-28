1b6d3e91a4f61d560fc02fa112efef8d1ba650c8
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-05-27 22:09:59 -0500

    Adds a published_on date to posts to make drafts more flexible.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327024956) do
+ActiveRecord::Schema.define(version: 20150528030118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20150327024956) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "is_published", default: false
+    t.date     "published_on"
   end
 
 end
