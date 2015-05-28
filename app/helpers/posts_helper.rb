8e613fdd85d6a6444327259116a3dbdc81c7d0a6
Sarah Wheeler <sarahcwheeler@gmail.com>
2015-05-27 22:10:38 -0500

    Starting to build out < and > nav arrows for blog posts.

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,17 @@
 module PostsHelper
+
+	def previous_entry(post_id)
+		post = Post.find(post_id)
+		# return all posts ordered from oldest to newest
+		# find post_id in the array and save the position
+		# if there is an element in the position before post_id
+			# return an arrow link to it
+		# else
+			# don't display an arrow/link
+
+	end
+
+	def next_entry(post_id)
+	end
+
 end
